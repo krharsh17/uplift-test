7790e88d381f3c6d68641f9a5a3ae741ce6fb996
Kumar Harsh <kharsh39@gmail.com>
2020-06-25 18:11:45 +0530

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+(function() {
+    const atOptions = {
+        'key' : '564f85513980c1c494e31cc2b785c5ae',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+    };
+
+    atOptions.foo = 'bar'
+}())
