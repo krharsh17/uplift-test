cf921db1e8c94b2b99effac938443ab7ddd2c99c
Kumar Harsh <kharsh39@gmail.com>
2020-06-25 23:25:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-(function() {
-    const atOptions = {
-        'key' : '564f85513980c1c494e31cc2b785c5ae',
-        'format' : 'iframe',
-        'height' : 250,
-        'width' : 300,
-        'params' : {}
-    };
-
-    atOptions.foo = 'bar'
-}())
+const atOptions = {
+    'key' : '564f85513980c1c494e31cc2b785c5ae',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+};
